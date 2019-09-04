e210153e6c5044336ab212f18c4c43ad203d1ba5
Yoshi Tadokoro <yossher@gmail.com>
2019-09-04 21:29:22 +0900

    イベント時に非同期通信

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,11 @@
 $('#user-search-field').on('keyup', function(e){
     var input = $("#user-search-field").val();
 
+    $.ajax({
+      type: 'GET',                
+      url:  '/users',             
+      data: { keyword: input},    
+      dataType: 'json'            
+  })
 
 });
