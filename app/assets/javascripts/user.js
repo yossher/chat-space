4790c0266e90a905e7d9ee41a961b222576231b2
Yoshi Tadokoro <yossher@gmail.com>
2019-09-04 21:36:50 +0900

    非同期通信の結果を得てHTML作成2

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,3 +1,24 @@
+$(document).on('turbolinks:load', function() {
+  var search_list = $("#user-search-result");
+  function appendUser(user) {
+    var html =
+    `
+    <div id="user-search-result">
+    <div class = "chat-group-user js-group-user clearfix">
+      <p class = "chat-group-user__name">${user.name}</p>
+      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+    </div>
+    </div>
+    `
+    search_list.append(html);
+  }
+
+  function appendNoUser(user) {
+    var html =
+    `<div class = "chat-group-user clearfix">${user}</div>`
+    search_list.append(html);
+  }
+
 $('#user-search-field').on('keyup', function(e){
     var input = $("#user-search-field").val();
 
