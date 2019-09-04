8dfe7943a22515a82b63ec51479edfe71ab45939
Yoshi Tadokoro <yossher@gmail.com>
2019-09-04 21:34:49 +0900

    非同期通信の結果を得てHTML作成1

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,4 +8,23 @@ $('#user-search-field').on('keyup', function(e){
       dataType: 'json'            
   })
 
+  .done(function(users){     
+
+      if (input.length === 0) {
+          $('#user-search-result').empty();
+        }
+
+      else if (input.length !== 0){
+          $('#user-search-result').empty();
+          users.forEach(function(user){ 
+              appendUser(user)
+          });
+      }
+
+      else {
+          $('#user-search-result').empty();
+          appendNoUser("一致するユーザーが見つかりません");
+      }
+  })
+
 });
