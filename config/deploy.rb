3df9890f3fe98a866c04984cae12e78445f91d7e
Yoshi Tadokoro <yossher@gmail.com>
2019-10-08 10:45:23 +0900

    最終修正（グループ内メンバーの全員の表示、グループ更新の機能）

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,7 +58,6 @@ set :default_env, {
 set :linked_files, %w{ config/secrets.yml }
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
